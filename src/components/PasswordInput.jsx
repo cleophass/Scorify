20aa278aef54811c2dddf62647d3a7ba90ed1489
cleophass <cleophas.fournier@efrei.net>
2024-04-03 11:30:33 +0200

    on peut passer de signin a signup

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-import {
-    
-    EyeIcon,
-    EyeSlashIcon,
-    
-  } from '@heroicons/react/24/outline';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 
 const PasswordInput = ({ label }) => {
   const [isHidden, setIsHidden] = useState(true);
@@ -14,19 +9,20 @@ const PasswordInput = ({ label }) => {
   };
 
   return (
-    <div className="flex flex-col text-left">
+    <div className="flex flex-col text-left style={{ width: '400px' }}">
       <label className="mb-2 text-sm font-medium text-gray-700">{label}</label>
       <div className="relative">
         <input
           type={isHidden ? 'password' : 'text'}
-          className="w-full rounded-md border border-stroke bg-transparent pr-10 px-5 py-3 text-base text-body-color outline-none focus:border-primary focus-visible:shadow-none dark:border-dark-3 dark:text-white"
+          style={{ width: '400px' }}
+          className="w-full rounded-md border bg-transparent pr-10 px-5 py-3 text-base text-body-color outline-none focus:border-custom-blue focus:ring-0 dark:border-dark-3 dark:text-white"
           placeholder={label}
         />
         <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3" onClick={toggleVisibility}>
           {isHidden ? (
-            <EyeSlashIcon className="h-5 w-5 bg-white" />
+            <EyeSlashIcon className="h-5 w-5 text-gray-500 hover:text-blue-600" />
           ) : (
-            <EyeIcon className="h-5 w-5 bg-transparent" />
+            <EyeIcon className="h-5 w-5 text-gray-500 hover:text-blue-600" />
           )}
         </button>
       </div>
